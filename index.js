c62e6c7696ee3d6707a0ccf5a2b7436b39cbc0c6
peuldev <arfinpeul523@gmail.com>
2024-01-25 20:40:00 +0600

    user deleted

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 var cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -62,7 +62,19 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+    // all user get
+    app.get("/user", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
+    // deleted user
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
